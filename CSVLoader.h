9a174a733d55148bbd90eb6af7e53c6de268f226
michaelostr <michaelostrowski453@gmail.com>
2026-03-26 14:43:08 -0400

    Add CSVLoader interface for parsing dataset

--- CSVLoader.h
+++ CSVLoader.h
@@ -5,4 +5,20 @@
 #ifndef PROJECT2_GROUP114_CSVLOADER_H
 #define PROJECT2_GROUP114_CSVLOADER_H
 
+#include <string>
+#include <vector>
+#include "HealthRecord.h"
+
+class CSVLoader {
+public:
+    static std::vector<HealthRecord> loadHealthData(const std::string& filename);
+private:
+    static std::vector<std::string> splitCSVLine(const std::string& line);
+    static std::string trim(const std::string& str);
+    static HealthRecord parseRecord(const std::vector<std::string>& fields);
+
+    static int parseInt(const std::string& value);
+    static double parseDouble(const std::string& value);
+};
+
 #endif //PROJECT2_GROUP114_CSVLOADER_H
